ecbb59392fc86a15d549d4908c54afb3d6081d8f
zohresalimi <zohre.1987@gmail.com>
2020-06-07 19:23:43 +0200

    circle timer

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-const durationInput = document.querySelector('#duration')
-const startBtn = document.querySelector('#start')
-const pauseBtn = document.querySelector('#pause')
-const circle = document.querySelector('circle')
+const durationInput = document.querySelector("#duration")
+const startBtn = document.querySelector("#start")
+const pauseBtn = document.querySelector("#pause")
+const circle = document.querySelector("circle")
 const perimeter = circle.getAttribute("r") * 2 * Math.PI
 
-circle.setAttribute('stroke-dasharray', perimeter)
-let totalDuration = 3;
+circle.setAttribute("stroke-dasharray", perimeter)
+let totalDuration = durationInput.value
 const timer = new Timer(durationInput, startBtn, pauseBtn, {
-    onStart(duration) {
-        totalDuration = totalDuration || duration
-    },
-    onTick(timeRemaining) {
-        circle.setAttribute('stroke-dashoffset',
-            perimeter * timeRemaining / totalDuration - perimeter
-        )
-    },
-    onPause() { console.log('Timer Paused') },
-    onComplete() { console.log('Timer Completed') }
+	onStart(duration) {
+		totalDuration = totalDuration || duration
+	},
+	onTick(timeRemaining) {
+		circle.setAttribute(
+			"stroke-dashoffset",
+			(perimeter * timeRemaining) / totalDuration - perimeter
+		)
+	},
+	onPause() {
+		console.log("Timer Paused")
+	},
+	onComplete() {
+		console.log("Timer Completed")
+	},
 })
